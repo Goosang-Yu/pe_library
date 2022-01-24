bb6a6a93092dae8dc0eb9d14852543b6a7d64948
Goosang Yu <gsyu93@gmail.com>
2022-01-24 16:30:08 +0900

    update group_56_classify_ver220124.py

--- source_code/group_56_classify_ver220124.py
+++ source_code/group_56_classify_ver220124.py
@@ -24,12 +24,56 @@ def main():
     for MM_num in list_mismatch_num:
         print('Filter: Mismatch number', MM_num)
         df_by_mm_num = df_raw.loc[df_raw['MM'] == MM_num]
+        df_by_mm_num.reset_index(inplace=True, drop=True)
+        # df_by_mm_num.to_csv('%s/MM%s_df_by_MM.csv' % (InputDIR, MM_num)) ## for test / delete later
         print(df_by_mm_num)
 
-        list_brcd = list(df_by_mm_num['Brcd'].drop_duplicates())
+        dict_cPE = {}
+        dict_cBG = {}
+        dict_out = {}
 
-        for brcd in list_brcd:
-            data = df_by_mm_num.loc[brcd]
+        for idx, brcd in tqdm(enumerate(df_by_mm_num['Brcd']),
+                              desc='Make dictionaries', ncols=100, total=len(df_by_mm_num)):
+
+            data = df_by_mm_num.loc[idx]
+
+            if brcd not in dict_out:
+                dict_cPE[brcd] = {}
+                dict_cBG[brcd] = {}
+                dict_out[brcd] = {'Trgt': brcd, 'PBS': data['PBS'], 'RTT': data['RTT'], 'MM': data['MM'],
+                                  'posEdit': data['posEdit'], 'On-trgt': data['On-trgt'], 'WTSeq': data['WTSeq']}
+                d = dict_out[brcd]
+                temp_CltSeq = list(d['WTSeq'][:21] + d['On-trgt'][21:21 + d['RTT']] + d['WTSeq'][21 + d['RTT']:])
+                temp_CltSeq[25] = 'c' ## pos +5 G to C transversion
+                d['CompltEditSeq'] = ''.join(temp_CltSeq)
+
+            dict_cPE[brcd][data['RefSeq']] = data['cPE']
+            dict_cBG[brcd][data['RefSeq']] = data['cBG']
+
+        for brcd in dict_out:
+            d = dict_out[brcd]
+            ## read count for PE-treated
+            tot_cPE = sum(list(dict_cPE[brcd].values()))
+            otr_cPE = dict_cPE[brcd]['Other']
+            WT_cPE  = dict_cPE[brcd][d['WTSeq']]
+            clt_cPE = dict_cPE[brcd][d['CompltEditSeq']]
+            par_cPE = tot_cPE - otr_cPE - WT_cPE - clt_cPE
+
+            ## read count for BG
+            tot_cBG = sum(list(dict_cBG[brcd].values()))
+            otr_cBG = dict_cBG[brcd]['Other']
+            WT_cBG  = dict_cBG[brcd][d['WTSeq']]
+            clt_cBG = dict_cBG[brcd][d['CompltEditSeq']]
+            par_cBG = tot_cBG - otr_cBG - WT_cBG - clt_cBG
+
+            ## Calculate editing efficiency
+
+
+
+
+
+
+
 
 
 
