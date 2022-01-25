956d37055bbd99e8490a087b6bcb5693e55d5110
Goosang Yu <gsyu93@gmail.com>
2022-01-25 19:55:54 +0900

    update group_56_classify_ver220124.py

--- source_code/group_56_classify_ver220124.py
+++ source_code/group_56_classify_ver220124.py
@@ -1,7 +1,6 @@
 import sys
 import time
 import pandas as pd
-import numpy as np
 from tqdm import tqdm
 
 def main():
@@ -18,19 +17,16 @@ def main():
 
     list_mismatch_num = list(df_raw['MM'].drop_duplicates())
 
-    print(df_raw)
-    print(list_mismatch_num)
-
     for MM_num in list_mismatch_num:
-        print('Filter: Mismatch number', MM_num)
+        print('-' * 50, '\nFilter: Mismatch number', MM_num, '\n', '-' * 50)
         df_by_mm_num = df_raw.loc[df_raw['MM'] == MM_num]
         df_by_mm_num.reset_index(inplace=True, drop=True)
         # df_by_mm_num.to_csv('%s/MM%s_df_by_MM.csv' % (InputDIR, MM_num)) ## for test / delete later
-        print(df_by_mm_num)
 
         dict_cPE = {}
         dict_cBG = {}
         dict_out = {}
+        list_err = []
 
         for idx, brcd in tqdm(enumerate(df_by_mm_num['Brcd']),
                               desc='Make dictionaries', ncols=100, total=len(df_by_mm_num)):
@@ -54,19 +50,27 @@ def main():
 
         for brcd in dict_out:
             d = dict_out[brcd]
-            ## read count for PE-treated
-            tot_cPE = sum(list(dict_cPE[brcd].values()))
-            otr_cPE = dict_cPE[brcd]['Other']
-            WT_cPE  = dict_cPE[brcd][d['WTSeq']]
-            clt_cPE = dict_cPE[brcd][d['CompltEditSeq']] ### key error for complt edit seq -> 아마도 cltSeq 만드는 code가 잘못된듯
-            par_cPE = tot_cPE - otr_cPE - WT_cPE - clt_cPE
+            try:
+                ## read count for PE-treated
+                tot_cPE = sum(list(dict_cPE[brcd].values()))
+                otr_cPE = dict_cPE[brcd]['Other']
+                WT_cPE  = dict_cPE[brcd][d['WTSeq']]
+                clt_cPE = dict_cPE[brcd][d['CompltEditSeq']] ### key error for complt edit seq -> 아마도 cltSeq 만드는 code가 잘못된듯
+                par_cPE = tot_cPE - otr_cPE - WT_cPE - clt_cPE
+            except:
+                error = ['PE', brcd, d['WTSeq'], d['CompltEditSeq'], d['On-trgt']]
+                list_err.append(error)
 
             ## read count for BG
-            tot_cBG = sum(list(dict_cBG[brcd].values()))
-            otr_cBG = dict_cBG[brcd]['Other']
-            WT_cBG  = dict_cBG[brcd][d['WTSeq']]
-            clt_cBG = dict_cBG[brcd][d['CompltEditSeq']]
-            par_cBG = tot_cBG - otr_cBG - WT_cBG - clt_cBG
+            try:
+                tot_cBG = sum(list(dict_cBG[brcd].values()))
+                otr_cBG = dict_cBG[brcd]['Other']
+                WT_cBG  = dict_cBG[brcd][d['WTSeq']]
+                clt_cBG = dict_cBG[brcd][d['CompltEditSeq']]
+                par_cBG = tot_cBG - otr_cBG - WT_cBG - clt_cBG
+            except:
+                error = ['BG', brcd, d['WTSeq'], d['CompltEditSeq'], d['On-trgt']]
+                list_err.append(error)
 
             ## Calculate editing efficiency
 
@@ -74,7 +78,10 @@ def main():
             woOther_BG = tot_cBG - otr_cBG
             woOther_PE = tot_cPE - otr_cPE
 
-            BG_All_Edit_Effi = 100 * (woOther_PE * ((clt_cBG + par_cBG) / woOther_BG))
+            if woOther_BG:
+                BG_All_Edit_Effi = 100 * ((clt_cBG + par_cBG) / woOther_BG)
+            else:
+                BG_All_Edit_Effi = 0
             PE_clt_only_effi = 0
             PE_Par_only_effi = 0
             PE_All_edit_effi = 0
@@ -98,20 +105,48 @@ def main():
                 PE_All_edit_effi = (((clt_cBG + par_cBG) - norm_All) / (woOther_PE - norm_All)) * 100
 
             # make output
+
+            d['Total_cPE'] = tot_cPE
+            d['Other_cPE'] = otr_cPE
+            d['woOther_cPE'] = woOther_PE
+            d['WTSeq_cPE'] = WT_cPE
+            d['Complete_cPE'] = clt_cPE
+            d['Partial_cPE'] = par_cPE
+
+            d['Total_cBG'] = tot_cBG
+            d['Other_cBG'] = otr_cBG
+            d['woOther_cBG'] = woOther_BG
+            d['WTSeq_cBG'] = WT_cBG
+            d['Complete_cBG'] = clt_cBG
+            d['Partial_cBG'] = par_cBG
+
             d['BG_AllEdit']   = BG_All_Edit_Effi # complete + partial edit in BG sample
             d['PE_CmpltEdit'] = PE_clt_only_effi # complete edit (expected product) fraction
             d['PE_PartlEdit'] = PE_Par_only_effi # partial edit (unexpected product; Not WT or complete edit)
             d['PE_AllEdit']   = PE_All_edit_effi # complete + partial edit
 
-        print('MM test')
+            list_mismatch_pos = []
 
+            for pos in range(0, len(d['On-trgt'])):
+                on_base = d['On-trgt'][pos]
+                mm_base = d['WTSeq'][pos]
+                if on_base != mm_base: list_mismatch_pos.append(pos)
+
+            counted_mm = len(list_mismatch_pos)
+            mm_pos_info = ''
+            for p in list_mismatch_pos:
+                mm_pos_info = '%s%s_' % (mm_pos_info, p)
+
+            d['Counted MM number'] = counted_mm
+            d['MMpos'] = mm_pos_info
+
+        df_error = pd.DataFrame(list_err, columns=['Sample Type', 'Barcode', 'WTSeq', 'CompleteEditSeq', 'On-target'])
+        df_error.to_csv('%s/error_list_MM%s.csv' % (OutputDIR, MM_num), index=False)
         df_result = pd.DataFrame(dict_out).T
-        print(df_result)
+        df_result.to_csv('%s/output_MM%s.csv' % (OutputDIR, MM_num), index=False)
 
         # for loop end: dict_out
     # for loop end: list_mismatch_num
-
-
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
